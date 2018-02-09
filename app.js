8c32517aeb1534b296e8062e367136d1e7139c4a
David Vezzani <david_vezzani@yahoo.com>
2018-02-09 03:15:41 -0700

    must have /api to translate to right port

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/api', days);
-app.use('/', auth);
+app.use('/api', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
